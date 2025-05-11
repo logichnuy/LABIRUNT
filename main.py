1edb95ca8941631ac6cd9a499c45b77a28bfb8f6
Vislyy <maksimslepuch@gmail.com>
2025-05-11 16:22:35 +0300

    9

---
+++ main.py
@@ -0,0 +1,71 @@
+import pygame
+
+class BaseSprite():
+    def __init__(self,x, y, texture, speed, w, h):
+        self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, [ w, h])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+    def draw(self, window):
+        window.blit(self.texture, self.hitbox)
+
+
+class Hero(BaseSprite):
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.hitbox.x += self.speed
+
+
+class Wall:
+    def __init__(self,x, y, texture, speed, w, h):
+        self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, [ w, h])
+        self.hitbox = self.texture.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.color = color
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.hitbox)
+
+
+pygame.init()
+walls = [
+    Wall(115, 80, [255, 0, 0], 100, 20)
+    
+
+    ]
+
+window = pygame.display.set_mode([700, 500])
+clook = pygame.time.Clock()
+background_img = pygame.image.load("background.jpg")
+background_img = pygame.transform.scale(background_img, [700, 500])
+hero = Hero(250, 250, "hero.png", 2, 50, 50)
+
+
+
+,
+
+
+
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    hero.update()
+
+
+
+    window.fill([143,  123, 181  ])
+    window.blit(background_img, [0,0])
+    hero.draw(window)
+    pygame.display.flip()
+
+    clook.tick(60)
